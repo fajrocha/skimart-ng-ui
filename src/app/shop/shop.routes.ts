74f76843b270b2c915a2439711235262c0c46c94
faroc-wsl-desktop <faroc@wsl-desktop.com>
2024-01-31 00:27:10 +0000

    Basic setup of dependencies and routing.

---
+++ src/app/shop/shop.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+export const SHOP_ROUTES: Routes = [
+  {
+    path: '',
+    component: ShopComponent,
+  },
+  {
+    path: ':id',
+    component: ProductDetailsComponent,
+  },
+];
